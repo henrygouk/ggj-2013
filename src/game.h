bc0b715ad66deb3d21180704678ecc9564e6d354
Henry Robbins Gouk <henry.gouk@gmail.com>
2013-01-25 20:37:18 +1300

    Added stuff

---
+++ src/game.h
@@ -0,0 +1,11 @@
+#ifndef GAME_H
+#define GAME_H
+
+extern bool running;
+
+void initialise();
+void cleanup();
+void update();
+void draw();
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,50 @@
 
 using namespace std;
 
+bool running;
+
 int main(int argc, char **argv)
 {
-	cout << "Game thing." << endl;
+	//Set running to true so the game loop does something
+	running = true;
+	
+	//Init various things that need initing
+	initialise();
+	
+	//The game loop
+	while(true)
+	{
+		//Update and draw
+		update();
+		draw();
+	}
+	
+	//Clean up
+	cleanup();
 
 	return 0;
+}
+
+//Initialise things that need something done at startup
+void initialise()
+{
+
+}
+
+//Clean up resources
+void cleanup()
+{
+
+}
+
+//Update game logic
+void update()
+{
+
+}
+
+//Draw the world
+void draw()
+{
+
 }
