dbc4f7c3d87d29cc68fb4f6e1de35eeeb532e944
Henry Robbins Gouk <henry.gouk@gmail.com>
2013-01-26 01:28:01 +1300

    Do commit

--- src/PlayerObject.cpp
+++ src/PlayerObject.cpp
@@ -5,6 +5,13 @@ using namespace sf;
 PlayerObject::PlayerObject(Vector2f pos)
 {
 	position = pos;
+	
+	sf::Image img;
+	img.LoadFromFile("assets/Bounce-1sm.png");
+	
+	images.push_back(img);
+	
+	sprite.FlipX(true);
 }
 
 void PlayerObject::update()
@@ -12,15 +19,20 @@ void PlayerObject::update()
 	if(window->GetInput().IsKeyDown(Key::Right))
 	{
 		position.x += 5.0;
+		
+		sprite.FlipX(true);
 	}
 	else if(window->GetInput().IsKeyDown(Key::Left))
 	{
 		position.x -= 5.0;
+		
+		sprite.FlipX(false);
 	}
 }
 
 void PlayerObject::draw()
 {
+	sprite.SetImage(images[0]);
 	sprite.SetPosition(position);
 	window->Draw(sprite);
 }
